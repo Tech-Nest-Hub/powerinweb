6facfe6133de4606dabcf7ed0d8ff3e32c74c4bc
Ghost9841 <asubedi778@gmail.com>
2025-09-17 20:40:42 +0545

    Dark and white theme

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from './theme/theme-provider'
+import { ModeToggle } from './theme/mode-toggle'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-  <>
-  </>
+    <div>
+       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+    </ThemeProvider>
+    </div>
   )
 }
 
