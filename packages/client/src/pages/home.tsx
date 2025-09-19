b53518990436427411e63b2d4e9c1ba6c6744f1a
Ghost9841 <asubedi778@gmail.com>
2025-09-19 12:57:22 +0545

    Added lucide react and search

--- packages/client/src/pages/home.tsx
+++ packages/client/src/pages/home.tsx
@@ -1,8 +1,11 @@
+import { Search } from "lucide-react";
+
 const Home = () => {
   return (
     <>
       <h1>Welcome to Our Website</h1>
       <p>This is the home page.</p>     
+     <Search />
     </>
   );
 }
