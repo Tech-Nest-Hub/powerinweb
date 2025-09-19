2b6cd58b347d44d445e382660ad9f96566790f06
Ghost9841 <asubedi778@gmail.com>
2025-09-19 22:46:30 +0545

    Website create, get is done, just added some controller no middleware is configured

--- packages/server/routes/website_routes.ts
+++ packages/server/routes/website_routes.ts
@@ -1,10 +1,10 @@
 
 import express, { type Request, type Response } from "express";
+import { createWebsite, getWebsites } from "../controllers/websiteController.ts";
 
 const website_router = express.Router();
 
-website_router.get('/websites', (req:Request, res:Response) => {
-    res.send('websites');
-});
+website_router.get('/websites', getWebsites)
+website_router.post('/websites', createWebsite)
 
 export default website_router;
