2a45a70a3fcb4bc4eeb221e5d7ceca9fb247410e
Ghost9841 <asubedi778@gmail.com>
2025-09-19 12:25:54 +0545

    Added a base route and a web route

--- packages/server/routes/website_routes.ts
+++ packages/server/routes/website_routes.ts
@@ -1,2 +1,10 @@
-import { app } from "../index.ts";
 
+import express, { type Request, type Response } from "express";
+
+const website_router = express.Router();
+
+website_router.get('/websites', (req:Request, res:Response) => {
+    res.send('websites');
+});
+
+export default website_router;
