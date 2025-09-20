93bec19646e219d7512a1de10b2c6f7b9c2b6417
Ghost9841 <asubedi778@gmail.com>
2025-09-20 16:13:52 +0545

    Fixed

--- packages/client/src/pages/website/components/HeroSectionWebsiteCarousel.tsx
+++ packages/client/src/pages/website/components/HeroSectionWebsiteCarousel.tsx
@@ -1,14 +1,15 @@
 
 import * as React from "react"
 import Autoplay from "embla-carousel-autoplay"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "../../../components/ui/card"
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "../../../components/ui/carousel"
+
 
 const HeroSectionWebsiteCarousel = () => {
   const plugin = React.useRef(
@@ -17,31 +18,26 @@ const HeroSectionWebsiteCarousel = () => {
   return (
     <Carousel
       plugins={[plugin.current]}
-      className="w-full  px-5"
+      className="w-full rounded-none mt-0"
       onMouseEnter={plugin.current.stop}
       onMouseLeave={plugin.current.reset}
     >
-      <CarouselContent>
+      <CarouselContent className="">
         {Array.from({ length: 5 }).map((_, index) => (
-              <>
-          <CarouselItem key={index} className="basis-full">
-              <div className="p-1">
-                <Card>
+          <>
+            <CarouselItem key={index} className="basis-full rounded-none">
+              <Card className="rounded-none py-6 m-0 ">
 
-                  <CardContent className="flex w-full h-[400px] items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-
-                </Card>
-                                                                <CarouselPrevious />
-      <CarouselNext />
-              </div>
+                <CardContent className="flex w-full h-[400px] items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
             </CarouselItem>
-</>
+          </>
         ))}
-        
       </CarouselContent>
-  
+      <CarouselNext className="right-4 size-12" />
+      <CarouselPrevious className="left-4 size-12" />
     </Carousel>
   )
 }
--- packages/client/src/pages/website/pages/ViewWebsitePage.tsx
+++ packages/client/src/pages/website/pages/ViewWebsitePage.tsx
@@ -3,7 +3,6 @@ import HeroSectionCarousel from "../components/HeroSectionWebsiteCarousel";
 const Websites = () => {
   return (
     <>
-      <h1>Our Websites</h1>
       <HeroSectionCarousel />
       <p>This is the websites page.</p>
     </>
