e82228fb768a2f7142baecde1bcbde370ce5915d
Ghost9841 <asubedi778@gmail.com>
2025-09-20 08:57:57 +0545

    added cors and some changes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,10 +3,15 @@ import website_router from "./routes/website_routes.ts";
 import project_router from "./routes/project_routes.ts";
 import tag_router from "./routes/tag_routes.ts";
 import category_router from "./routes/category_routes.ts";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
+app.use(cors());
 
 app.use(express.json());
 
@@ -20,6 +25,6 @@ app.get('/', (req : Request, res: Response) => {
 })
 
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
