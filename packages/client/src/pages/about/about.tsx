90da6a56f3cab93092475e9210b59da80779e929
Pratyush827 <tamrakarpratyush7@gmail.com>
2025-09-22 21:51:44 +0545

    i m sorry don't mean it, can't control the status of life

--- packages/client/src/pages/about/about.tsx
+++ packages/client/src/pages/about/about.tsx
@@ -1,9 +1,36 @@
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the about page.</p>
-    </div>
+    <>
+      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
+        <div className="w-150 px-10 mx-10">
+          <h1 className="text-4xl text-bold py-4">Techspire</h1>
+          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+        </div>
+        <div className="p-10 m-10">
+          <img src="" alt="techspire"></img>
+        </div>
+      </div>
+
+      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
+        <div className="p-10 m-10">
+          <img src="" alt="aakash subedi"></img>
+        </div>
+        <div className="w-150 px-10 mx-10">
+          <h1 className="text-4xl text-bold py-4">Aakash Subedi</h1>
+          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+        </div>
+      </div>
+
+      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
+        <div className="w-150 px-10 mx-10">
+          <h1 className="text-4xl text-bold py-4">Pratyush Tamrakar</h1>
+          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+        </div>
+        <div className="p-10 m-10">
+          <img src="" alt="pratyush tamrakar"></img>
+        </div>
+      </div>
+    </>
   );
 }
 
