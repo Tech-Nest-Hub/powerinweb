d41dba6a7e11193dc581702c32abc01de26faa9e
Pratyush827 <tamrakarpratyush7@gmail.com>
2025-09-23 08:42:44 +0545

    basic strucutre of about page

--- packages/client/src/pages/about/about.tsx
+++ packages/client/src/pages/about/about.tsx
@@ -1,33 +1,110 @@
+import { Instagram } from 'lucide-react'
+import { Linkedin } from 'lucide-react';
+import { Github } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+
 const About = () => {
   return (
     <>
-      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
-        <div className="w-150 px-10 mx-10">
-          <h1 className="text-4xl text-bold py-4">Techspire</h1>
-          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+      <h1 className="text-center text-7xl px-4 py-2">About Us</h1>
+
+      <h1 className="text-center text-6xl px-4 py-4 mb-5">Driven By Passion, Guided By Innovation!</h1>
+      
+      <div className="flex flex-wrap justify-around my-10">
+        <div className="w-[450px]">
+          <h1 className="text-4xl my-5 text-bold">Technest</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores cupiditate nisi cum nesciunt ratione excepturi quisquam at commodi fuga quod quia, labore in id ut dignissimos nam sint impedit. Rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sequi obcaecati id odit exercitationem sapiente vel repudiandae dicta, voluptas sint dolor enim! Alias omnis eaque molestiae accusamus delectus, asperiores quod?</p>
         </div>
-        <div className="p-10 m-10">
-          <img src="" alt="techspire"></img>
+
+        <div className="w-[250px] h-[300px] ">
+          <img src="" alt="tech-nest: the future of tech "/>
+        </div>
+
+        <div>
+          <div >
+            <h1 className="text-5xl">11+</h1>
+            <h1 className="text-2xl">Projects Done</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">1+</h1>
+            <h1 className="text-2xl">Years of Experience</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">10+</h1>
+            <h1 className="text-2xl">Programming Languages</h1>
+          </div>
+        </div>
+
+        <div className="">
+          <Link to="https://www.github.com" className="m-4 p-2"><Github color="#000000ff" size={40} /></Link>
+          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
+          <Link to="https://www.instagram.com" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
         </div>
       </div>
 
-      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
-        <div className="p-10 m-10">
-          <img src="" alt="aakash subedi"></img>
+      <div className="flex flex-wrap justify-around my-10">
+        <div className="w-[450px]">
+          <h1 className="text-4xl my-5 text-bold">Aakash Subedi</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores cupiditate nisi cum nesciunt ratione excepturi quisquam at commodi fuga quod quia, labore in id ut dignissimos nam sint impedit. Rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sequi obcaecati id odit exercitationem sapiente vel repudiandae dicta, voluptas sint dolor enim! Alias omnis eaque molestiae accusamus delectus, asperiores quod?</p>
         </div>
-        <div className="w-150 px-10 mx-10">
-          <h1 className="text-4xl text-bold py-4">Aakash Subedi</h1>
-          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+
+        <div className="w-[250px] h-[300px] ">
+          <img src="" alt="tech-nest: the future of tech "/>
+        </div>
+
+        <div>
+          <div >
+            <h1 className="text-5xl">21+</h1>
+            <h1 className="text-2xl">Projects Done</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">4+</h1>
+            <h1 className="text-2xl">Years of Experience</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">10+</h1>
+            <h1 className="text-2xl">Programming Languages</h1>
+          </div>
+        </div>
+
+        <div className="">
+          <Link to="https://www.github.com" className="m-4 p-2"><Github color="#000000ff" size={40} /></Link>
+          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
+          <Link to="https://www.instagram.com" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
         </div>
       </div>
 
-      <div className="px-4 py-4 mx-20 my-10  pt-10 align-center flex justify-between h-[400px] rounded-lg shadow shadow-lg">
-        <div className="w-150 px-10 mx-10">
-          <h1 className="text-4xl text-bold py-4">Pratyush Tamrakar</h1>
-          <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nobis aperiam atque libero eum voluptatum ullam, nesciunt officia distinctio sunt esse laudantium consequuntur nostrum laborum delectus omnis totam corporis.</p>
+
+      <div className="flex flex-wrap justify-around my-10">
+        <div className="w-[450px]">
+          <h1 className="text-4xl my-5 text-bold">Pratyush Tamrakar</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores cupiditate nisi cum nesciunt ratione excepturi quisquam at commodi fuga quod quia, labore in id ut dignissimos nam sint impedit. Rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sequi obcaecati id odit exercitationem sapiente vel repudiandae dicta, voluptas sint dolor enim! Alias omnis eaque molestiae accusamus delectus, asperiores quod?</p>
         </div>
-        <div className="p-10 m-10">
-          <img src="" alt="pratyush tamrakar"></img>
+
+        <div className="w-[250px] h-[300px] ">
+          <img src="" alt="tech-nest: the future of tech "/>
+        </div>
+
+        <div>
+          <div >
+            <h1 className="text-5xl">4+</h1>
+            <h1 className="text-2xl">Projects Done</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">2+</h1>
+            <h1 className="text-2xl">Years of Experience</h1>
+          </div>
+          <div>
+            <h1 className="text-5xl">5+</h1>
+            <h1 className="text-2xl">Programming Languages</h1>
+          </div>
+        </div>
+
+        <div className="">
+          <Link to="https://www.github.com" className="m-4 p-2"><Github color="#000000ff" size={40} /></Link>
+          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
+          <Link to="https://www.instagram.com" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
         </div>
       </div>
     </>
