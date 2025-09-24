3b2784316c9300edb2d5b5d3c46e7de0048bc7e5
Pratyush827 <tamrakarpratyush7@gmail.com>
2025-09-24 19:26:30 +0545

    styling about us

--- packages/client/src/pages/about/about.tsx
+++ packages/client/src/pages/about/about.tsx
@@ -1,26 +1,35 @@
-import { Instagram , Linkedin , Github , Youtube } from 'lucide-react'
+import { Instagram, Linkedin, Github, Youtube } from 'lucide-react'
 import { Link } from 'react-router-dom';
+import technest from '@/assets/technest.jpg'
 
 const About = () => {
   return (
     <>
       <h1 className="text-center text-7xl px-4 py-2 mt-8 font-extrabold ">About Us</h1>
 
-      <h1 className="text-center text-5xl px-4 py-4 mb-8 font-bold">Driven By Passion,<br className='mb-4'/> Guided By Innovation!</h1>
-      
-      <div className="flex flex-wrap justify-around my-10 shadow shadow-lg">
-        <div className="w-[450px]">
-          <h1>TechNest</h1>
+      <h1 className="text-center text-5xl px-4 py-4 mb-8 font-bold">Driven By Passion,<br className='mb-4' /> Guided By Innovation!</h1>
+
+      <div className="flex flex-wrap flex-row justify-around my-10 shadow shadow-lg">
+        <div className="flex flex-row ">
+          <div className="flex flex-col items-center">
+            <h1 className="text-4xl ">TechNest</h1>
+            <div className="flex flex-row">
+              <Link to="https://github.com/Tech-Nest-Hub" className="m-4 p-2"><Github className='text-black dark:text-white' size={40} /></Link>
+              <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40} /></Link>
+              <Link to="https://www.youtube.com/@technest-0" className="m-4 p-2"><Youtube size={40} color="#ff0000" /></Link>
+              <Link to="https://www.instagram.com/_tech.nest__/" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
+            </div>
+          </div>
+          <img src={technest} alt="technest " style={{height:50}} />
+
+        </div>
+
+        <div className="w-[500px] px-10 mx-5">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus corrupti mollitia sed. Eaque magni consequatur ipsum eum saepe. Doloremque necessitatibus, doloribus autem distinctio illum deleniti repudiandae quia minima similique veniam.</p>
         </div>
 
 
 
-        <div className="flex flex-row">
-          <Link to="https://github.com/Tech-Nest-Hub" className="m-4 p-2"><Github className='text-black dark:text-white' size={40} /></Link>
-          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
-          <Link to="https://www.youtube.com/@technest-0" className="m-4 p-2"><Youtube size={40} color="#ff0000" /></Link>
-          <Link to="https://www.instagram.com/_tech.nest__/" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
-        </div>
       </div>
 
       <div className="flex flex-wrap justify-around my-10 shadow shadow-lg">
@@ -30,7 +39,7 @@ const About = () => {
         </div>
 
         <div className="w-[250px] h-[300px] ">
-          <img src="" alt="ghost "/>
+          <img src="" alt="ghost " />
         </div>
 
         <div>
@@ -50,7 +59,7 @@ const About = () => {
 
         <div className="">
           <Link to="https://github.com/Ghost9841" className="m-4 p-2"><Github className='text-black dark:text-white' size={40} /></Link>
-          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
+          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40} /></Link>
           <Link to="https://www.instagram.com" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
         </div>
       </div>
@@ -63,7 +72,7 @@ const About = () => {
         </div>
 
         <div className="w-[250px] h-[300px] ">
-          <img src="" alt="tech-nest: the future of tech "/>
+          <img src="" alt="tech-nest: the future of tech " />
         </div>
 
         <div>
@@ -83,7 +92,7 @@ const About = () => {
 
         <div className="">
           <Link to="https://github.com/Pratyush827" className="m-4 p-2"><Github className='text-black dark:text-white' size={40} /></Link>
-          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40}/></Link>
+          <Link to="https://www.linkedin.com" className="m-4 p-2"><Linkedin color="#00b3ff" size={40} /></Link>
           <Link to="https://www.instagram.com" className="m-4 p-2"><Instagram size={40} color="#e22222" /></Link>
         </div>
       </div>
