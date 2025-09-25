b7789797f3f1046cf1f9f0bf1bf29887ebae4545
Ghost9841 <asubedi778@gmail.com>
2025-09-25 19:48:35 +0545

    featured section

--- packages/client/src/pages/website/components/FeaturedSectionWebsite.tsx
+++ packages/client/src/pages/website/components/FeaturedSectionWebsite.tsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react"
 import { type WebsiteType } from "../types/website_types";
 import getWebsites from "@/api/websites";
-import { Button } from "@/components/ui/button";
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const FeaturedSectionWebsite = () => {
     const [websites, setWebsites] = useState<WebsiteType[]>([]);
@@ -25,6 +33,28 @@ const FeaturedSectionWebsite = () => {
            {websites.map((website) => (
                 <h1 key={website.id}>{website.name}</h1>
            ))}
+            <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm"
+    >
+        <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
         </div>
 
     </div>
